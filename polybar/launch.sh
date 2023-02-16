4fb5bd7c656547d417724855abc5abf310add278
Iliyan Teofilov <iliyanvt18@gmail.com>
2023-02-16 23:56:44 +0100

    Modified polybar config for desktop

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -6,8 +6,16 @@ killall -q polybar
 # Launch Polybar, using default config location ~/.config/polybar/config.ini
 polybar main 2>&1 | tee -a /tmp/polybar.log &
 
-if [[ $(xrandr -q | grep 'HDMI-1-0 connected') ]]; then
-    polybar external &
+if [[ $(xrandr -q | grep 'eDP-1 connected') ]]; then
+    polybar laptop &
+fi
+
+if [[ $(xrandr -q | grep 'DisplayPort-0 connected') ]]; then
+    polybar desktop-main &
+fi
+
+if [[ $(xrandr -q | grep 'HDMI-A-0 connected') ]]; then
+    polybar desktop-right &
 fi
 
 disown
