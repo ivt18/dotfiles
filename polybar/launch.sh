fabf09937705159eb2bc9fcd61addc6caa109316
Iliyan Teofilov <iliyanvt18@gmail.com>
2023-05-18 11:11:11 +0200

    Improved loading of polybar depending on connected monitors

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -4,10 +4,18 @@
 killall -q polybar
 
 # Launch Polybar, using default config location ~/.config/polybar/config.ini
-polybar laptop 2>&1 | tee -a /tmp/polybar.log &
+if [[ $(xrandr -q | grep 'eDP-1 connected') ]]; then
+    polybar laptop 2>&1 | tee -a /tmp/polybar.log &
+    if [[ $(xrandr -q | grep 'HDMI-1-0 connected') ]]; then
+        polybar external 2>&1 | tee -a /tmp/polybar.log &
+    fi
+fi
 
-if [[ $(xrandr -q | grep 'HDMI-1-0 connected') ]]; then
-    polybar external &
+if [[ $(xrandr -q | grep 'DisplayPort-0 connected') ]]; then
+    polybar desktop-main 2>&1 | tee -a /tmp/polybar.log &
+    if [[ $(xrandr -q | grep 'HDMI-A-0 connected') ]]; then
+        polybar desktop-right 2>&1 | tee -a /tmp/polybar.log &
+    fi
 fi
 
 disown
